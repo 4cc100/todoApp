3ebb3a0f3deafe0b92e48e4c251254c2e490c7e5
4cc100 <vikcsi.varga@gmail.com>
2018-12-30 14:38:13 +0100

    remove mongoClient/mongodb

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 require('dotenv').load();
 
 const express = require('express');
-const { MongoClient } = require('mongodb');
 
 const app = express();
 
-
-function getMongoClient(callback) {
-  MongoClient.connect(process.env.DB_URI, { useNewUrlParser: true }, (err, db) => {
-    if(err) {
-      throw new Error(err);
-    } else {
-
-      const clientPlatfrom = db.topology.clientInfo.platform;
-      const dbName = db.s.options.db;
-      console.info(`Successfully connected to ${dbName} database from the ${clientPlatfrom} clientplatform...`);
-
-      callback(db);
-    }
-  });
-}
-
-function getDB() {
-  getMongoClient((mongoClient) => mongoClient.db(mongoClient.s.options.db));
-}
-getDB();
-
-function disconnect() {
-  return mongoClient.close();
-}
 
 app.get('/', (req, res) => {
   res.send("hi there!");
