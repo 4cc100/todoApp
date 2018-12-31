cb9e254d226f1c11c2d73e6c919b564ea94c005e
4cc100 <vikcsi.varga@gmail.com>
2018-12-31 22:34:44 +0100

    code clean up

--- routes/todos.js
+++ routes/todos.js
@@ -7,60 +7,9 @@ router.route('/')
 .get(helpers.getTodos)
 .post(helpers.createTodo);
 
-// router.get('/', (req, res) => {
-//   db.Todo.find()
-//     .then((todos) => {
-//       res.json(todos)
-//     })
-//     .catch((err) => {
-//       res.send(`The following error occured: ${err}`);
-//     });
-// });
-
-// router.post('/', (req, res) => {
-//   db.Todo.create(req.body)
-//   .then((newTodo) => {
-//     res.status(201).json(newTodo);
-//   })
-//   .catch((err) => {
-//     res.send(`The following error occured: ${err}`);
-//   });
-// });
-
-
 router.route('/:todoId')
 .get(helpers.getTodo)
 .put(helpers.updateTodo)
 .delete(helpers.removeTodo);
-
-// router.get('/:todoId', (req, res) => {
-//   db.Todo.findById(req.params.todoId)
-//   .then((foundTodo) => {
-//     res.json(foundTodo);
-//   })
-//   .catch((err) => {
-//     res.send(`The following error occured: ${err}`);
-//   });
-// });
-
-// router.put('/:todoId', (req, res) => {
-//   db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-//   .then((todo) => {
-//     res.json(todo);
-//   })
-//   .catch((err) => {
-//     res.send(`The following error occured: ${err}`);
-//   });
-// });
-
-// router.delete('/:todoId', (req, res) => {
-//   db.Todo.remove({_id: req.params.todoId})
-//   .then(()=> {
-//     res.json({message: 'We deleted it'});
-//   })
-//   .catch((err) => {
-//     res.send(`The following error occured: ${err}`);
-//   });
-// });
 
 module.exports = router;
